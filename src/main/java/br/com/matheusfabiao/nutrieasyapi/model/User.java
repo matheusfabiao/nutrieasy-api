0a004da0ee8afbe87cc775170f282bbe9ee6d62f
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-10 16:37:12 -0300

    user model att

--- src/main/java/br/com/matheusfabiao/nutrieasyapi/model/User.java
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/model/User.java
@@ -1,15 +1,23 @@
 package br.com.matheusfabiao.nutrieasyapi.model;
 
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
-@EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String password;
+    private LocalDateTime createdAt;
+
 }

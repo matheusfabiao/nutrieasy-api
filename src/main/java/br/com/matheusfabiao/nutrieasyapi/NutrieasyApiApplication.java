7e21a2bc77215279418a030b7941b322fbb16d82
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-10 15:34:16 -0300

    Initial commit

---
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/NutrieasyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusfabiao.nutrieasyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrieasyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrieasyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusfabiao/nutrieasyapi/NutrieasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusfabiao.nutrieasyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrieasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

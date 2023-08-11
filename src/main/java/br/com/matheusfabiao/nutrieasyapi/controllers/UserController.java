efac04130baf8e5a0c52b37a57d1a1947aaa0079
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-11 00:54:12 -0300

    add user findById method in service layer

--- src/main/java/br/com/matheusfabiao/nutrieasyapi/controllers/UserController.java
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/controllers/UserController.java
@@ -1,9 +1,11 @@
 package br.com.matheusfabiao.nutrieasyapi.controllers;
 
+import br.com.matheusfabiao.nutrieasyapi.dto.UserDTO;
 import br.com.matheusfabiao.nutrieasyapi.dto.UserMinDTO;
 import br.com.matheusfabiao.nutrieasyapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping(value = "/{id}")
+    public UserDTO findById(@PathVariable Long id){
+        return userService.findById(id);
+    }
 
     @GetMapping
     public List<UserMinDTO> findAll(){
--- src/main/java/br/com/matheusfabiao/nutrieasyapi/services/UserService.java
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/services/UserService.java
@@ -1,10 +1,12 @@
 package br.com.matheusfabiao.nutrieasyapi.services;
 
+import br.com.matheusfabiao.nutrieasyapi.dto.UserDTO;
 import br.com.matheusfabiao.nutrieasyapi.dto.UserMinDTO;
 import br.com.matheusfabiao.nutrieasyapi.entities.User;
 import br.com.matheusfabiao.nutrieasyapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,10 +14,16 @@ import java.util.List;
  * Service class for managing user-related operations.
  */
 @Service
+@Transactional(readOnly = true)
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public UserDTO findById(Long id){
+        User result = userRepository.findById(id).get();
+        return new UserDTO(result);
+    }
 
     /**
      * Retrieves a list of UserMinDTO objects containing minimal user information.

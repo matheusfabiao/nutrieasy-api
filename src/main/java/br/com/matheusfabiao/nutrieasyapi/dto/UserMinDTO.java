5521a650492f24acdfc7fac82fbe1c6dfdbf7fc2
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-10 23:35:39 -0300

    add user entity and min dto docstrings

--- src/main/java/br/com/matheusfabiao/nutrieasyapi/dto/UserMinDTO.java
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/dto/UserMinDTO.java
@@ -6,15 +6,39 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * Data Transfer Object (DTO) representing a minimal version of the User entity.
+ * This DTO is used to efficiently transfer user data between layers of the application.
+ */
 @Getter
 @NoArgsConstructor
 public class UserMinDTO {
 
+    /**
+     * The unique identifier for the user.
+     */
     private Long id;
+
+    /**
+     * The full name of the user.
+     */
     private String name;
+
+    /**
+     * The email address of the user.
+     */
     private String email;
+
+    /**
+     * The timestamp indicating when the user was created.
+     */
     private LocalDateTime createdAt;
 
+    /**
+     * Constructs a UserMinDTO object based on the provided User entity.
+     *
+     * @param entity The User entity from which to construct the DTO.
+     */
     public UserMinDTO(User entity) {
         id = entity.getId();
         name = entity.getName();
--- src/main/java/br/com/matheusfabiao/nutrieasyapi/entities/User.java
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/entities/User.java
@@ -8,6 +8,11 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a user entity in the NutriEasy API.
+ * This class is annotated as an entity for database mapping.
+ * Uses Lombok annotations for getter, setter, and constructor generation.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,12 +21,31 @@ import java.time.LocalDateTime;
 @Table(name = "tb_user")
 public class User {
 
+    /**
+     * The unique identifier for the user.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * The full name of the user.
+     */
     private String name;
+
+    /**
+     * The email address of the user.
+     */
     private String email;
+
+    /**
+     * The password associated with the user.
+     */
     private String password;
+
+    /**
+     * The timestamp indicating when the user was created.
+     */
     private LocalDateTime createdAt;
 
 }

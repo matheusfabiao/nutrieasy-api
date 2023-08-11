455be6f8289fdc2a90f24b75d92d994e6eecc0cb
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-11 00:30:38 -0300

    add import.sql for testing

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+-- Users for testing
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 1', 'user1@example.com', 'password1', '2023-08-11 09:00:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 2', 'user2@example.com', 'password2', '2023-08-11 10:30:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 3', 'user3@example.com', 'password3', '2023-08-11 12:15:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 4', 'user4@example.com', 'password4', '2023-08-11 14:45:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 5', 'user5@example.com', 'password5', '2023-08-11 16:30:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 6', 'user6@example.com', 'password6', '2023-08-11 18:00:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 7', 'user7@example.com', 'password7', '2023-08-11 19:45:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 8', 'user8@example.com', 'password8', '2023-08-11 21:15:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 9', 'user9@example.com', 'password9', '2023-08-11 22:30:00');
+INSERT INTO tb_user (name, email, password, created_at) VALUES ('User 10', 'user10@example.com', 'password10', '2023-08-11 23:45:00');

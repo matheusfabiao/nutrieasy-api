1967d96447890e793abe399d50b94e1aed3be4a3
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-10 23:52:25 -0300

    add user controller layer

---
+++ src/main/java/br/com/matheusfabiao/nutrieasyapi/controllers/UserController.java
@@ -0,0 +1,24 @@
+package br.com.matheusfabiao.nutrieasyapi.controllers;
+
+import br.com.matheusfabiao.nutrieasyapi.entities.User;
+import br.com.matheusfabiao.nutrieasyapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> findAll(){
+        List<User> result = userService.findAll();
+        return result;
+    }
+}
